0a161116e67f87cce715c7b3e4f1b992c3bc4520
Sir Devi0us <S1rDev10us@outlook.com>
2022-03-09 11:12:18 +0000

    added index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    <style>
+      
+      :root {
+        font-family: "Comic Sans MS", "Comic Sans", cursive;
+        color:aqua;
+        
+        background-color:#420;
+      }
+      
+    </style>
+  </head>
+  <body>
+    <?php $data = file_get_contents ("visits.json")+1;
+      file_put_contents ('visits.json',$data);
+      echo '<h1>This site has been visited ';
+      echo $data;
+      echo ' times</h1>'
+    ?>
+    
+    <h3>He is life</h3>
+    <?php echo '<p>Hello World</p>'; ?> 
+
+  </body>
+</html>
