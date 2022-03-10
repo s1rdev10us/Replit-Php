c185466e0c722967f4665a53879e2207235c9c1b
Sir Devi0us <S1rDev10us@outlook.com>
2022-03-10 13:34:14 +0000

    added a missing filepath

--- php-scripts/include.php
+++ php-scripts/include.php
@@ -1,5 +1,5 @@
 <?php
-	require $_SERVER['DOCUMENT_ROOT'].'/functionsAndVariables/root.php';
+	require $_SERVER['DOCUMENT_ROOT'].'/php-scripts/functionsAndVariables/root.php';
 	require $php-root.'/functionsAndVariables/endsWith.php';
 	require $php-root.'/functionsAndVariables/consoleLog.php';
 ?>
