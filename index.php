5d94d5d73863f3269cb0fe95e148bd73d5eecbb5
Sir Devi0us <S1rDev10us@outlook.com>
2022-03-10 08:07:58 +0000

    added a visitor counter to the title

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <!DOCTYPE php>
 <html lang="en">
 	<head>
-		<title>PHP Test</title>
+		<title>
+			<?php
+				$window = parse_url($_SERVER['HTTP_HOST']);
+				consoleLog($window);
+				$window=$window["path"];
+				consoleLog($window);
+				function endsWith($string, $endString)
+				{
+					$len = strlen($endString);
+					if ($len == 0) {
+						return true;
+						}
+					return (substr($string, -$len) === $endString);
+				}
+				if(endsWith($window,'repl.co')) {
+					$window='source-replit';
+				}
+				$jsonString = file_get_contents($jsonFile);
+				$data = json_decode($jsonString, true);
+				$data = $data[window]+1
+				echo "This site has had {$data} visitors";
+
+			?>
+		</title>
 		<style>
 	  
 			:root {
--- php-scripts/visitorCounter/visitorCounter.php
+++ php-scripts/visitorCounter/visitorCounter.php
@@ -21,10 +21,11 @@
 		$window='source-replit';
 	}
 	consoleLog($window);
-	$jsonString = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/php-scripts/visitorCounter/visitorCounter.json');
+	$jsonFile = $_SERVER['DOCUMENT_ROOT'].'/php-scripts/visitorCounter/visitorCounter.json';
+	$jsonString = file_get_contents($jsonFile);
 	$data = json_decode($jsonString, true);
 	$data[$window]=$data[$window]+1;
 	echo "<h1>This site has been visited {$data[$window]} times</h1>";
 	$newJsonString = json_encode($data);
-	file_put_contents($_SERVER['DOCUMENT_ROOT'].'/php-scripts/visitorCounter/visitorCounter.json', $newJsonString);
+	file_put_contents($jsonFile, $newJsonString);
 ?>
